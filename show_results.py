ac2e6958cc904af6ba12d9e98d7b445ae32ae83d
XiaowanYi <48965338+XiaowanYi@users.noreply.github.com>
2019-07-21 11:28:28 +0800

    Update show_results.py

--- show_results.py
+++ show_results.py
@@ -9,12 +9,12 @@ Original file is located at
 
 import pandas as pd
 
-#canidae_path = 'single_att_results/canidae.csv'
+canidae_path = 'single_att_results/canidae.csv'
 kitchen_path = 'single_att_results/kitchen.csv'
 
-#df_canidae = pd.read_csv(canidae_path, index_col = 0)
+df_canidae = pd.read_csv(canidae_path, index_col = 0)
 df_kitchen = pd.read_csv(kitchen_path, index_col = 0)
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    #print(df_canidae)
+    print(df_canidae)
     print(df_kitchen)
