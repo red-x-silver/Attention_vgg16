0e00b2df3130f8bf34d346e8f27bd17bcf3e6930
XiaowanYi <48965338+XiaowanYi@users.noreply.github.com>
2019-07-29 15:17:14 +0800

    Update show_results.py

--- show_results.py
+++ show_results.py
@@ -12,7 +12,7 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import matplotlib
-matplotlib.use("Agg")
+#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 file_name = 'single_att_results/'
