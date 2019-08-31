e081065773843d79dec6ed462d026b88d2be91ec
XiaowanYi <48965338+XiaowanYi@users.noreply.github.com>
2019-09-01 00:45:39 +0100

    Update ctgry_stats_inspect.py

--- ctgry_stats_inspect.py
+++ ctgry_stats_inspect.py
@@ -14,17 +14,30 @@ matplotlib.use("Agg")
 
 import pandas as pd
 import numpy as np
-
+import math
 
 ctgry_list = ['ave', 'canidae', 'cloth', 'felidae', 'kitchen', 'land_trans']
 imagenet_train = '/mnt/fast-data16/datasets/ILSVRC/2012/clsloc/train/'
+
+def weighted_avg_and_std(values, weights):
+    """
+    Return the weighted average and standard deviation.
+
+    values, weights -- Numpy ndarrays with the same shape.
+    """
+    average = numpy.average(values, weights=weights)
+    # Fast and numerically precise:
+    var = numpy.average((values-average)**2, weights=weights)
+    variance = var * len(weights)/(len(weights) - 1)
+    return (average, math.sqrt(variance))
+
 
 for ctgry in ctgry_list:
     class_csv_path = 'groupings-csv/' + ctgry + '_Imagenet.csv'
     df_classes = pd.read_csv(class_csv_path)
     df_classes["number_of_img"] = ""
     classes = [i for i in df_classes['wnid']]
-    weighted_acc = []
+    acc = []
     nimg_list = []
     for class_wid in classes:
         class_path = imagenet_train + class_wid
@@ -32,17 +45,17 @@ for ctgry in ctgry_list:
         nimg_list.append(n_img)
         #df_classes[df_classes['wnid'] == class_wid]['number_of_img'] = n_img
         acc = df_classes[df_classes['wnid']== class_wid]['base_accuracy'].values[0]
-        weighted_acc.append(acc*n_img)
-    weighted_acc = np.array(weighted_acc)
+        acc.append(acc)
+    acc = np.array(acc)
     nimg_list = np.array(nimg_list)
     #print ( weighted_acc)
     total_img = nimg_list.sum()  
+    weights = nimg_list/total_img
     #print (total_img)
-    weighted_acc = weighted_acc/total_img
-
+    avg, std = weighted_avg_and_std(acc, weights)
     print ('The total number of images for ' + ctgry + ':' + str(total_img))
-    print ('The mean of acc for ' + ctgry + ':' + str(weighted_acc.sum()))
-    print ('The std of acc for ' + ctgry + ':' + str(weighted_acc.std()))
+    print ('The mean of acc for ' + ctgry + ':' + str(avg))
+    print ('The std of acc for ' + ctgry + ':' + str(std))
     
     
     
